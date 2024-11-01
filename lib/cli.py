0eb3d5b54245ea1653293bb6578187c54a659105
Elvis <elvis.omondi@student.moringaschool.com>
2024-11-01 22:48:27 +0300

    -m

--- lib/cli.py
+++ lib/cli.py
@@ -1,6 +1,6 @@
 import click
 from database import init_db, SessionLocal
-from models import User
+from models import User, Task
 
 @click.group()
 def cli():
@@ -26,6 +26,25 @@ def list_users():
         click.echo(f'User ID: {user.id}, Name: {user.name}')
 
 @cli.command()
+@click.option('--title', prompt='Task Title', help='Title of the task.')
+@click.option('--user_id', prompt='User ID', type=int, help='ID of the user assigned to the task.')
+def add_task(title, user_id):
+    """Add a new task."""
+    db = SessionLocal()
+    new_task = Task(title=title, user_id=user_id)
+    db.add(new_task)
+    db.commit()
+    click.echo(f'Task "{title}" added for User ID {user_id}.')
+
+@cli.command()
+def list_tasks():
+    """List all tasks."""
+    db = SessionLocal()
+    tasks = db.query(Task).all()
+    for task in tasks:
+        click.echo(f'Task ID: {task.id}, Title: {task.title}, User ID: {task.user_id}')
+
+@cli.command()
 def init():
     """Initialize the database."""
     init_db()
@@ -33,3 +52,4 @@ def init():
 
 if __name__ == '__main__':
     cli()
+
